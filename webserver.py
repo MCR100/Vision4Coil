c9793006a34f2b1d258c6beeba04d72a57ca6b42
cf27n <nathan.croll123@gmail.com>
2026-03-19 02:45:55 +0000

    disable flask reloading for debug

--- webserver.py
+++ webserver.py
@@ -220,4 +220,4 @@ def mjpeg_frame():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, threaded=True)
+    app.run(host="0.0.0.0", port=8000, threaded=True, use_reloader=False)
